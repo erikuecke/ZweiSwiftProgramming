f44dbce0773d8bf31e42d8307326d38b23466489
Erik Uecke <erik.uecke@digitalfilmacademy.edu>
2017-03-07 10:53:06 -0500

    Chapter02 Types, Constants, and variables

---
+++ Swift02.playground/Contents.swift
@@ -0,0 +1,40 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+// TYPES, CONSTANTS, AND VARIABLES
+
+// TYPES
+
+// Assigning a string to a variable
+//var numberOfStopLights = "Four"
+
+// Add "Four" and 2
+//numberOfStopLights += 2
+
+// Using a numerical type
+//var numberOfStopLights: Int = 4
+//numberOfStopLights += 2
+
+// CONSTANTS AND VARIABLES
+
+// Declaring a constant
+let numberOfStopLights: Int = 4
+
+// Declaring Population
+var population: Int
+
+// Giving population a value
+population = 5422
+
+// STRING INTERPOLATION
+// Giving a town a name
+
+let townName: String = "Knowher"
+
+// Crafting the town description
+var townDescription = "\(townName) has a population of \(population) and \(numberOfStopLights) stoplights."
+
+// Bronze challenge
+var townUnemployment = 35
+townDescription += " Number of unemployed is \(townUnemployment)"
