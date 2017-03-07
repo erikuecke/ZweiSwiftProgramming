7f5049d6579c2d654ae5c08f01b6eb591b5d4edb
Erik Uecke <erik.uecke@digitalfilmacademy.edu>
2017-03-07 10:37:50 -0500

    Chapter01 Getting Started

--- Swift01.playground/Contents.swift
+++ Swift01.playground/Contents.swift
@@ -3,3 +3,17 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+// Varying Variables and Printing to the Console
+// Proper Punctuation
+str += "!"
+
+// Printing to the console
+print(str)
+
+// Debug area: Shift + Cmd + Y
+
+// Bronze Challenge
+let lastName = "Uecke"
+print(lastName)
+
